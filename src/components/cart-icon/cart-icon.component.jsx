4a54e0af4e7cc56440058810e55dd2bdea3aeb02
OsimFavour <famos204@gmail.com>
2024-10-22 10:30:59 +0100

    Add state to cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,19 @@
 
+import { useDispatch, useSelector } from 'react-redux'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 import './cart-icon.styles.scss'
+import { selectIsCartOpen } from '../../store/cart/cart.selector'
+import { setIsCartOpen } from '../../store/cart/cart.slice'
 
 const CartIcon = () => {
-    
+    const dispatch = useDispatch()
+    const isCartOpen = useSelector(selectIsCartOpen)
+
+    const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen))
 
     return (
-        <div className='cart-icon-container'>
+        <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon'/>
             <span className='item-count'>45</span>
         </div>
