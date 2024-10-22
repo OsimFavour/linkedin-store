d9e21ffe68eae4d05936467366a5326052e1ca41
OsimFavour <famos204@gmail.com>
2024-10-22 10:30:11 +0100

    Change cart icon state

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,39 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+
+import { selectIsCartOpen } from '../../store/cart/cart.selector'
 
 import { ReactComponent as LinkedInLogo } from '../../assets/linkedin.svg' 
 
 const Header = () => {
-  return (
-    <header className="header-area header-sticky">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <nav className="main-nav">
-                    
-                        <Link to="index.html" className="logo text-dark">
-                           <LinkedInLogo />
+
+    const isCartOpen = useSelector(selectIsCartOpen)
+    console.log('Open Cart menu>>>', isCartOpen);
+    
+
+    return (
+        <header className="header-area header-sticky background-header">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <nav className="main-nav">
+                        
+                            <Link to="index.html" className="logo text-dark">
+                            <LinkedInLogo />
+                                
+                            </Link>
+                            <ul className="nav">
+                                <li className="scroll-to-section"><Link to="/" className="active">Home</Link></li>
+                                <li className="scroll-to-section"><Link to="/shop">Shop</Link></li>
+                                <li className="scroll-to-section"><Link to="/sign-in">Sign In</Link></li>
                             
-                        </Link>
-                        <ul className="nav">
-                            <li className="scroll-to-section"><Link to="/" className="active">Home</Link></li>
-                            <li className="scroll-to-section"><Link to="/shop">Shop</Link></li>
-                            <li className="scroll-to-section"><Link to="/sign-in">Sign In</Link></li>
-                           
-                            
-                            <li className="scroll-to-section"><Link to="/cart">Cart Symb</Link></li>
-                            
-                        </ul>  
-                             
-                        <Link className='menu-trigger'>
-                            <span>Menu</span>
-                        </Link>
-                    </nav>
+                                
+                                <li className="scroll-to-section">
+                                    <Link>
+                                        <CartIcon />
+                                    </Link>
+                                </li>
+                                
+                            </ul>  
+                                
+                            <Link className='menu-trigger'>
+                                <span>Menu</span>
+                            </Link>
+
+                            {isCartOpen && <CartDropdown />}
+                        </nav>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Header
